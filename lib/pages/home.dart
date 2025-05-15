e048db311d8d05723b866ed434ca6ee29e0f0738
Icaro Keller <icarokeller1@gmail.com>
2025-05-15 20:07:25 -0300

    correcao de bug

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lbapoa/widgets/bottom_nav.dart';
 import 'package:lbapoa/widgets/app_bar.dart';
+import 'package:lbapoa/widgets/instagram_posts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
